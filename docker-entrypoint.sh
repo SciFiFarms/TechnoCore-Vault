1ca1da5daa5dff0f16a495aab44b138fe8664bcb
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2018-08-11 20:10:17 -0600

    Make vault's config dir setting be ENV settable.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -32,7 +32,7 @@ fi
 # VAULT_CONFIG_DIR isn't exposed as a volume but you can compose additional
 # config files in there if you use this image as a base, or use
 # VAULT_LOCAL_CONFIG below.
-VAULT_CONFIG_DIR=/vault/config
+VAULT_CONFIG_DIR=${VAULT_CONFIG_DIR:-/vault/config}
 
 # You can also set the VAULT_LOCAL_CONFIG environment variable to pass some
 # Vault configuration JSON without having to bind any volumes.
